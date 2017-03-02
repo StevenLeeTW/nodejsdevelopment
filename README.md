324a0d34b6bdd6100bdbed8c642cd59900799c3d
JirLu <s1365789@gmail.com>
2017-03-02 16:43:40 +0800

    ch1~ch17 update

---
+++ README.md
@@ -0,0 +1,5 @@
+# nodejsdevelopment
+ch1 ~ ch17 version
+Implementing the book web development with node and express writen by EthanRBrown.
+I found something wrong and fixed it. 
+If you have any problem, mail me. 
---
+++ credentials.js
@@ -0,0 +1,13 @@
+module.exports = {
+	mongo: {    
+			development: {        
+			connectionString: 'MongoDBURL',},
+			production: {        
+			connectionString: 'MongoDBURL',},
+			    	     }, 	
+	cookieSecret: 'you cookie secret goes here',
+	gmail: {
+			user: 'your gmail username',
+			password: 'your gmail password',
+	}
+};
