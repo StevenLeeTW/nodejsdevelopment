0c47450867f71e2a5e6a24d854683e360ddd03f6
JirLu <s1365789@gmail.com>
2017-03-08 11:59:40 +0800

    ch18 update_1

---
+++ credentials.js
@@ -0,0 +1,13 @@
+module.exports = {
+  	mongo: {    
+  			development: {        
+ 			connectionString: 'mongodb://jirlu:s223569@ds063140.mlab.com:63140/ddatebase',},
+  			production: {        
+ 			connectionString: 'mongodb://jirlu:s223569@ds063140.mlab.com:63140/ddatebase',},
+  			    	     }, 	
+  	cookieSecret: 'you cookie secret goes here',
+  	gmail: {
+ 			user: 'your gmail username',
+ 			password: 'your gmail password',
+ 	}
+ };
